bb0233265b048fc589491e357352661ab3b05b42
Sebastian <oslobar@gmail.com>
2018-11-28 06:26:38 +0100

    Initial commit

---
+++ mobile/src/main/java/inc/intima/riobiohultsfred/MainActivity.java
@@ -0,0 +1,27 @@
+package inc.intima.riobiohultsfred;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://riobiohultsfred.se/");
+
+
+    }
+}
